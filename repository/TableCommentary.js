e4cbe922ef1c50c01588b18ef3bf154fcc0f9d03
himanshuZiwotech <himanshu@ziwotech.com>
2023-10-17 15:30:09 +0530

    fix  bugs

--- repository/TableCommentary.js
+++ repository/TableCommentary.js
@@ -375,7 +375,6 @@ const getCommentaryPlayersQuery = async (data, fastify, request) => {
       "wrPlayerName" as "playerName"
       from "tblCommentaryPlayers" tcp
       left join "tblEncryptedData" te on tcp."wrPlayerId" = te."wrKey"
-      left join "tblPlayers" tp on tcp."wrPlayerId" = tp."wrPlayerId"
       where "wrCommentaryId" = (select "wrKey" from "tblEncryptedData" where "wrValue" = $1) and "wrTeamId" = (select "wrKey" from "tblEncryptedData" where "wrValue" = $2)
       order by "wrDisplayOrder"
       `,
