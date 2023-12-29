3481d57c26cf25ef5251431a58ddb3bf2b86fadc
Nitesh <nitesh@ziwotech.com>
2023-12-29 23:21:04 +0530

    Player2

--- services/player.js
+++ services/player.js
@@ -121,7 +121,7 @@ const insertPlayerService = async (request, fastify) => {
       const jsonString = JSON.stringify(hashString);
       console.log(jsonString);
       // Convert the string back to an array of values
-      const hashArray = jsonString?.[0].split(",");
+      const hashArray = jsonString.split(",");
       console.log(hashArray);
       // for (let team of request.body.teamId) {
       //   const checkTeamId = global.tblTeams.find((item) => item.teamId === team);
