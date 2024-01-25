f5e04511aa67228d2dfcd717d8b5e49ed4cce261
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-01-25 12:52:37 +0400

    add role-permission in page,pageFormates

--- routes/admin/pageFormate/index.js
+++ routes/admin/pageFormate/index.js
@@ -1,4 +1,4 @@
-const { authorize } = require("../../../controller/middleware");
+const { authorize, checkPermission } = require("../../../controller/middleware");
 const {
   getAllPageFormats,
   getPageFormatById,
@@ -11,19 +11,37 @@ const { PageFormate } = require("../../../swaggerSchema/groupTags/schema");
 module.exports = async (fastify, opts) => {
   fastify.post("/all", {
     schema: PageFormate.getAll.schema,
-    preHandler: (request, reply) => authorize(request, reply, fastify),
+    preHandler: [
+      (request, reply) => authorize(request, reply, fastify),
+      (request, reply, done) => checkPermission(request, reply, fastify, {
+        tabName: "Page Formates",
+        mode: "view",
+      }),
+    ],
     handler: (request, reply) => getAllPageFormats(request, reply, fastify),
   });
 
   fastify.post("/byId", {
     schema: PageFormate.getById.schema,
-    preHandler: (request, reply) => authorize(request, reply, fastify),
+    preHandler: [
+      (request, reply) => authorize(request, reply, fastify),
+      (request, reply, done) => checkPermission(request, reply, fastify, {
+        tabName: "Page Formates",
+        mode: "view",
+      }),
+    ],
     handler: (request, reply) => getPageFormatById(request, reply, fastify),
   });
 
   fastify.post("/save", {
     schema: PageFormate.update.schema,
-    preHandler: (request, reply) => authorize(request, reply, fastify),
+    preHandler: [
+      (request, reply) => authorize(request, reply, fastify),
+      (request, reply, done) => checkPermission(request, reply, fastify, {
+        tabName: "Page Formates",
+        mode: request.body.pageFormatId == "0" ? "add" : "edit",
+      }),
+    ],
     handler: (request, reply) => savePageFormat(request, reply, fastify),
   });
 
