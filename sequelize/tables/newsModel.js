27ca95d6fe2cbbaa57f7575262dc6bf45643ab8c
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-02-24 12:04:31 +0530

    set up news Module API

---
+++ sequelize/tables/newsModel.js
@@ -0,0 +1,63 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const NewsModel = sequelize.define(
+    "tblTeam",
+    {
+      wrNewsId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      wrTitle: {
+        type: DataTypes.STRING(1000),
+        allowNull: true,
+      },
+      wrNews: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      wrImage: {
+        type: DataTypes.STRING(200),
+        allowNull: true,
+      },
+      wrIsPermanent: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      wrStartDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      wrEndDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      wrIsActive : {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      wrCreatedDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      wrCreatedBy: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      wrModifyBy: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      wrModifyDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return NewsModel;
+};
