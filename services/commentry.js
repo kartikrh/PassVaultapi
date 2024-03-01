777f39b781ae36182278ac16719f8112524b8d1d
Nitesh <nitesh@ziwotech.com>
2024-03-01 12:28:42 +0530

    Update_COMMENRTY_playerID

--- services/commentry.js
+++ services/commentry.js
@@ -3095,7 +3095,7 @@ const getInningDataByInningNumber = async (commentaryId, inningNumber) => {
 
   const bat1 = commentaryPlayers_batter.map((player) => {
     return {
-      pid: player.playerId,
+      pid: player.commentaryPlayerId,
       btn: player.playerName,
       ot: player.isBatterOut ? "OUT" : "NOT OUT",
       rt: player.isBatterRetir ? "RET" : "",
@@ -3118,7 +3118,7 @@ const getInningDataByInningNumber = async (commentaryId, inningNumber) => {
 
   const bow2 = commentaryPlayers_bowler.map((player) => {
     return {
-      pid: player.playerId,
+      pid: player.commentaryPlayerId,
       pln: player.playerName,
       ovr: player.bowlerOver || 0,
       mov: player.bowlerMaidenOver || 0,
@@ -3186,7 +3186,7 @@ const getInningDataByInningNumber = async (commentaryId, inningNumber) => {
 
   const bat2 = commentaryPlayers_batter2.map((player) => {
     return {
-      pid: player.playerId,
+      pid: player.commentaryPlayerId,
       btn: player.playerName,
       ot: player.isBatterOut ? "OUT" : "NOT OUT",
       rt: player.isBatterRetir ? "RET" : "",
@@ -3208,7 +3208,7 @@ const getInningDataByInningNumber = async (commentaryId, inningNumber) => {
 
   const bow1 = commentaryPlayers_bowler2.map((player) => {
     return {
-      pid: player.playerId,
+      pid: player.commentaryPlayerId,
       pln: player.playerName,
       ovr: player.bowlerOver || 0,
       mov: player.bowlerMaidenOver || 0,
