b3ba9eb25a3de3cbb12dbcadc1cce9ac39036534
Nitesh <nitesh@ziwotech.com>
2024-04-05 17:38:20 +0530

    addtcolor

--- repository/TableTeams.js
+++ repository/TableTeams.js
@@ -43,7 +43,7 @@ const insertTeamQuery = async (data, fastify, request) => {
     const result = await fastify.db.query(
       `with insert_data as(
       INSERT INTO "tblTeams" ("wrTeamName","wrTeamShortName", "wrImage", "wrCountry", "wrEventTypeId", "wrCreatedBy", "wrCreatedDate" , "WrTeamJersey","wrTeamColor")
-      VALUES ($1,$2,$3,$4,$5,$6,$7 , $8)
+      VALUES ($1,$2,$3,$4,$5,$6,$7 , $8,$9)
       RETURNING *    
     )
     SELECT 
