e1a79a94ef36b62c32176d5c0167d6ca4a8fcf53
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-05-15 20:29:57 +0530

    add teammaxover

--- services/commentry.js
+++ services/commentry.js
@@ -310,6 +310,7 @@ const createCommentaryService = async (request, fastify) => {
     }
   }
 
+  request.body.teamMaxOver = null;
   if (request.body.matchTypeId) {
     const validateMatchTypeId = global.tblMatchTypes.find(
       (item) => item.matchTypeId === request.body.matchTypeId
@@ -317,6 +318,7 @@ const createCommentaryService = async (request, fastify) => {
     if (!validateMatchTypeId) {
       throw new Error("MatchType with this id not Found");
     }
+    request.body.teamMaxOver = validateMatchTypeId.maxOversInFirstInings;
   }
 
   if (
@@ -818,6 +820,9 @@ const cloneCommentaryService = async (request, fastify) => {
   const validateMatchTypeId = global.tblMatchTypes.find(
     (item) => item.matchTypeId === originalCommentary.matchTypeId
   );
+  if(!validateMatchTypeId){
+    throw new Error("MatchType with this id not Found");
+  }
 
   const team1 = await getCommentaryTeamsQuery(
     { teamId: originalCommentary.team1Id, commentaryId },
@@ -864,6 +869,7 @@ const cloneCommentaryService = async (request, fastify) => {
     team2Captain: team2.teamCaptain,
     team2Kipper: team2.teamKipper,
     team2Players: filterOutUniquePlayerId(team2Players),
+    teamMaxOver: validateMatchTypeId.maxOversInFirstInings
   };
 
   if (validateMatchTypeId) {
