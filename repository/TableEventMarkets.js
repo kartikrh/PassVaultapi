903d5326661b390520a41481344320a3bf24e076
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-05-21 17:09:17 +0530

    add console for test

--- repository/TableEventMarkets.js
+++ repository/TableEventMarkets.js
@@ -1847,6 +1847,7 @@ const getMarketDataByCIdQuery = async (request, fastify) => {
 }
 const getMarketsByCIdQuery = async (request, fastify) => {
   try {
+    console.log("testing");
   const query = `
       WITH result_market_data AS (
         SELECT 
