cc15a56ef45aeb9184c46617d330f3fd0d1348d9
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-07-15 10:49:39 +0400

    bug ifx

--- services/eventMarket.js
+++ services/eventMarket.js
@@ -147,6 +147,7 @@ const getAllEventMarketsService = async (request, fastify) => {
   if(rateSourceRefId && rateSourceRefId != 0){
     createWhereStatus = `tem."wrRateSource" = ${rateSourceRefId}`;
   }
+  console.log("createWhereStatus",createWhereStatus);
   let eventMarket = await getAllEventMarketsQuery(fastify, createWhereStatus);
   if (eventTypeId) {
     // get the commentaryId from tblCommentaries
