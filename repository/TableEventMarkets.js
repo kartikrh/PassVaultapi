5fedf12ffc3a8331624d4cea73bf99a0f45e2bc3
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-07-18 10:44:28 +0400

    add console

--- repository/TableEventMarkets.js
+++ repository/TableEventMarkets.js
@@ -1095,6 +1095,7 @@ const cancelEventMarketByTeamIdQuery = async (data, request, fastify) => {
 
     return result;
   } catch (error) {
+    console.log(error);
     errorLogger(
       fastify,
       error.message,
