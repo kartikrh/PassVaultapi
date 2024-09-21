7c4ca13d5f2def15975620672c968fea11b839fc
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-09-21 11:33:33 +0400

    change in assignaward

--- repository/TableCommentaryAward.js
+++ repository/TableCommentaryAward.js
@@ -4,16 +4,20 @@ const getAllCommentaryAwardQuery = async (fastify) => {
     return await fastify.db.query(
         `
             SELECT 
-                "wrId" as "id",
-                "wrCommentaryId" as "commentaryId",
+                "tblCommentaryAwards"."wrId" as "id",
+                "tblCommentaryAwards"."wrCommentaryId" as "commentaryId",
                 "wrAwardId" as "awardId",
-                "wrTeamId" as "teamId",
-                "wrPlayerId" as "playerId",
-                "wrCreatedAt" as "createdAt",
-                "wrCreatedBy" as "createdBy",
-                "wrModifyAt" as "modifyAt",
-                "wrModifyBy" as "modifyBy"
+                "tblCommentaryAwards"."wrTeamId" as "teamId",
+                "tblCommentaryAwards"."wrPlayerId" as "playerId",
+                "tblCommentaryAwards"."wrCreatedAt" as "createdAt",
+                "tblCommentaryAwards"."wrCreatedBy" as "createdBy",
+                "tblCommentaryAwards"."wrModifyAt" as "modifyAt",
+                "tblCommentaryAwards"."wrModifyBy" as "modifyBy",
+                "team"."wrTeamName" as "teamName",
+                "player"."wrPlayerName" as "playerName"
             FROM "tblCommentaryAwards"
+            LEFT JOIN "tblPlayers" as "player" on "player"."wrPlayerId" = "tblCommentaryAwards"."wrPlayerId"
+            LEFT JOIN "tblTeams" as "team" on "team"."wrTeamId" = "tblCommentaryAwards"."wrTeamId"
             ORDER BY "wrId" DESC
         `,
         {
@@ -43,7 +47,11 @@ const addCommentaryAwardQuery = async (data,request,fastify) => {
             "wrCreatedAt" as "createdAt",
             "wrCreatedBy" as "createdBy",
             "wrModifyAt" as "modifyAt",
-            "wrModifyBy" as "modifyBy"
+            "wrModifyBy" as "modifyBy",
+            "team"."wrTeamName" as "teamName",
+            "player"."wrPlayerName" as "playerName"
+            left join "tblPlayers" as "player" on "player"."wrId" = "tblCommentaryAwards"."wrPlayerId"
+            left join "tblTeams" as "team" on "team"."wrId" = "tblCommentaryAwards"."wrTeamId"
         `;
 
         let values = [
@@ -141,10 +149,66 @@ const deleteCommentaryAwardQuery = async (request,fastify) => {
         throw new Error(error.message)
     }
 }
+const assignAwardQuery = async(data,request,fastify) => {
+    try {
+        // i want to get value (), () like this
+        let values = [];
+        data.forEach((item) => {
+            values.push(`(${item.commentaryId},${item.awardId},${item.teamId || null},${item.playerId || null},${request.userTokenInfo.WrUserId})`)
+        })
+        values = values.join(",");
+        
+
+        const query = `
+                WITH inserted AS (
+            INSERT INTO "tblCommentaryAwards"
+                (
+                    "wrCommentaryId",
+                    "wrAwardId",
+                    "wrTeamId",
+                    "wrPlayerId",
+                    "wrCreatedBy"
+                )
+                VALUES
+                ${values}  -- This is where your dynamically generated values go
+            RETURNING
+                "wrId" as "id",
+                "wrCommentaryId" as "commentaryId",
+                "wrAwardId" as "awardId",
+                "wrTeamId" as "teamId",
+                "wrPlayerId" as "playerId",
+                "wrCreatedAt" as "createdAt",
+                "wrCreatedBy" as "createdBy",
+                "wrModifyAt" as "modifyAt",
+                "wrModifyBy" as "modifyBy"
+        )
+        SELECT 
+            inserted.*,
+            team."wrTeamName" as "teamName",
+            player."wrPlayerName" as "playerName"
+        FROM inserted
+        LEFT JOIN "tblPlayers" player ON player."wrPlayerId" = inserted."playerId"
+        LEFT JOIN "tblTeams" team ON team."wrTeamId" = inserted."teamId";
+        `;  
+        const result = await fastify.db.query(query,{
+            type : fastify.db.QueryTypes.SELECT
+        })
+        return result;
+    } catch (error) {
+        errorLogger(
+            fastify,
+            error.message,
+            "DB ERROR --> repository/TableCommentaryAward/assignAward",
+            request
+        )
+        throw new Error(error.message)
+    }
+}
 
 module.exports = {
     getAllCommentaryAwardQuery,
     addCommentaryAwardQuery,
     updateCommentaryAwardQuery,
-    deleteCommentaryAwardQuery
+    deleteCommentaryAwardQuery,
+    assignAwardQuery
 }
