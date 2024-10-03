203af7ab00a2fc7d04d408054c7e308453c1b133
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-10-03 10:21:56 +0400

    revert change

--- services/competition.js
+++ services/competition.js
@@ -13,34 +13,17 @@ const { APIEndpointModuleType, ServiceType, callClientAPI } = require("../utilit
 
 const allCompetitionService = async (request) => {
   const { isActive, isTrending, eventTypeId } = request.body;
-  const result = global.tblCompetitions;
- 
-  if(eventTypeId) {
-    result = result.filter((item) => item.eventTypeId == eventTypeId);
-  }
-  if(isActive !== undefined){ 
-    result = result.filter((item) => item.isActive == isActive);
-  }
-  if(isActive == undefined){
-    result = result.filter((item) => item.isActive == true);
-  }
-  if(isTrending !== undefined) {
-    result = result.filter((item) => item.isTrending == isTrending);
-  }
 
-  return result;
-
-
-  // const filterObject = {
-  //   isActive: isActive,
-  //   isTrending: isTrending,
-  //   eventTypeId: eventTypeId === 0 ? null : eventTypeId,
-  // };
-  // // Additional checks for "0" and undefined
-  // filterObject.eventTypeId =
-  //   eventTypeId === 0 || eventTypeId === undefined
-  //     ? null
-  //     : filterObject.eventTypeId;
+  const filterObject = {
+    isActive: isActive,
+    isTrending: isTrending,
+    eventTypeId: eventTypeId === 0 ? null : eventTypeId,
+  };
+  // Additional checks for "0" and undefined
+  filterObject.eventTypeId =
+    eventTypeId === 0 || eventTypeId === undefined
+      ? null
+      : filterObject.eventTypeId;
 
   // if (isActive === undefined || isTrending === undefined) {
   //   const result = global.tblCompetitions.filter(
