244c6979e90e4b27e919c5cae6a654bf5213cb3c
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-11-06 11:45:28 +0400

    changes if ratediff

--- services/marketTemplate.js
+++ services/marketTemplate.js
@@ -192,7 +192,7 @@ const updateMarketTemplateService = async (request, fastify) => {
     beforeSuspendMin: request.body.beforeSuspendMin !== undefined ? parseInt(request.body.beforeSuspendMin) : marketTemplate.beforeSuspendMin,
     beforeCloseMin: request.body.beforeCloseMin !== undefined ? parseInt(request.body.beforeCloseMin) : marketTemplate.beforeCloseMin,
     defaultIsSendData: request.body.hasOwnProperty("defaultIsSendData") ? request.body.defaultIsSendData : marketTemplate.defaultIsSendData,
-    rateDiff: request.body.rateDiff !== undefined ? parseInt(request.body.rateDiff) : marketTemplate.rateDiff,
+    rateDiff: request.body.rateDiff !== undefined ? request.body.rateDiff : marketTemplate.rateDiff,
   };
   // update marketTemplate
   await updateMarketTemplateQuery(body, fastify, request);
