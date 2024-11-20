3d05f7e569703d8291f42788b4e138e1af5c2bfe
Nitesh <nitesh@ziwotech.com>
2024-11-20 14:08:32 +0530

    predictscore update

--- services/commentry.js
+++ services/commentry.js
@@ -2891,6 +2891,9 @@ const syncCommentaryStatsWithAPIAndSocket = async (request, fastify) => {
               strike_team_id: strikeTeam.teamId,
               wicket: _wkt === true ? 1 : 0,
               total_wicket: strikeTeam.teamWicket,
+              ball_by_ball_id: commentaryBallByBall.commentaryBallByBallId
+              ? parseInt(commentaryBallByBall.commentaryBallByBallId)
+              : null
             },
             "/api/v1/predictscore",
             fastify,
