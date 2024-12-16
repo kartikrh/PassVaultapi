557ed6246fc62bc92b724b3c38a2657e2adcb058
Jyoti-wpa <jyothi.wpa@gmail.com>
2024-12-16 17:51:56 +0400

    bug fix

--- services/commentry.js
+++ services/commentry.js
@@ -4071,7 +4071,8 @@ const updateTeamPlayerService = async (request, fastify) => {
       (item) =>
         item?.commentaryId === +commentaryId &&
         item.teamId === +teamId &&
-        item.playerId === +playerId
+        item.playerId === +playerId &&
+        item.currentInnings === +currentInnings
     );
     if (player) {
       player.batsmanStrikeRate = batsmanStrikeRate;
