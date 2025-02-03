2db32a8fd55964f4c5bd55ab496bfbe6e7626d3b
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-02-03 14:28:46 +0400

    api of markettype

--- controller/users/admin/marketTemplate/index.js
+++ controller/users/admin/marketTemplate/index.js
@@ -1,4 +1,4 @@
-const { saveMarketTemplateService, getAllMarketTemplateService, getMarketTemplateIdService, deleteMarketTemplateService, getMatchTypeListService, activeInactiveTemplateService, getByMatchTypeIdService, getMarketTypeListService, getCategoryByMarketTypeService, changePredefineRunnerService, cloneMarketTemplateService, getMarketTypeAndCategoryByMarketTypeService, isPerEventStatusService, isShowInAdvanceMarketChangeStatusService, cloneMultiMarketTemplateService, defaultIsSendDataChangeService, allMarketTypesAndCategoriesService } = require("../../../../services/marketTemplate");
+const { saveMarketTemplateService, getAllMarketTemplateService, getMarketTemplateIdService, deleteMarketTemplateService, getMatchTypeListService, activeInactiveTemplateService, getByMatchTypeIdService, getMarketTypeListService, getCategoryByMarketTypeService, changePredefineRunnerService, cloneMarketTemplateService, getMarketTypeAndCategoryByMarketTypeService, isPerEventStatusService, isShowInAdvanceMarketChangeStatusService, cloneMultiMarketTemplateService, defaultIsSendDataChangeService, allMarketTypesAndCategoriesService, mtAndCategoriesService } = require("../../../../services/marketTemplate");
 const { error, success, ERROR_CODES } = require("../../../../utilities");
 const { errorLogger } = require("../../../../utilities/logger");
 
@@ -161,6 +161,15 @@ const allMarketTypesAndCategories = async (request, reply, fastify) => {
     reply.status(200).send(error(err.message, ERROR_CODES.SERVER_ERROR, 200));
   }
 };
+const mtAndCategories = async (request, reply, fastify) => {
+  try {
+    const result = await mtAndCategoriesService(request ,fastify);
+    reply.status(200).send(success(result, 200));
+  } catch (err) {
+    errorLogger(fastify, err.message, commonPath + "/mtAndCategories", request);
+    reply.status(200).send(error(err.message, ERROR_CODES.SERVER_ERROR, 200));
+  }
+};
 module.exports = {
   getAllMarketTemplate,
   saveMarketTemplate,
@@ -179,4 +188,5 @@ module.exports = {
   cloneMultiMarketTemplate,
   defaultIsSendDataChange,
   allMarketTypesAndCategories,
+  mtAndCategories
 };
