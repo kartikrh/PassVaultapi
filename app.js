de9bfb0e2aca9f0046a98c897fb0fbb5ce66e282
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-03-26 17:59:23 +0400

    change in app

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const webPush = require("web-push");
 const {webPushset} = require("./WebPushHandler/index.js");
 const { updateMarket } = require("./utilities/marketUpdate.js");
 const cron = require('node-cron');
-
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 // Pass --options via CLI arguments in command to enable these options.
 module.exports.options = {};
 global.tblData = {};
@@ -46,6 +46,12 @@ if (process.env.ENABLE_SENTRY === "TRUE") {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1.0,
+    integrations: [
+      // Add our Profiling integration
+      nodeProfilingIntegration(),
+    ],
+    profileSessionSampleRate: 1.0,
+    profileLifecycle: 'trace',
   });
 }
 process.on('uncaughtException', (error) => {
