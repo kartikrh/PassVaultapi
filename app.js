4606f8dbc18660d5748c0e6f4f350ddc0737a1e1
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-03-27 15:45:22 +0400

    cange in app

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ if (process.env.ENABLE_SENTRY === "TRUE") {
     dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1.0,
     
-    integrations: [
+    // integrations: [
 
-      nodeProfilingIntegration(),
-      // ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-    ],
-    profileSessionSampleRate: 1.0,
-    profileLifecycle: 'trace',
+    //   nodeProfilingIntegration(),
+    //   // ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    // ],
+    // profileSessionSampleRate: 1.0,
+    // profileLifecycle: 'trace',
   });
 }
 process.on('uncaughtException', (error) => {
