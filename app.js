817ed8885e94b01aef85147ab9626aa08a7e069f
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-03-27 16:11:31 +0400

    change in app

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ if (process.env.ENABLE_SENTRY === "TRUE") {
     dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1.0,
     integrations : [
-      nodeProfilingIntegration(),
+      // nodeProfilingIntegration(),
       ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
     ],
     profileSessionSampleRate: 1.0,
