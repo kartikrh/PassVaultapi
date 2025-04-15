a26ce6084cb9dc16a68e48997479734517795482
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-04-15 10:47:17 +0400

    change in socket

--- socketIo.js
+++ socketIo.js
@@ -354,7 +354,7 @@ const connection = (socket , fastify) => {
   })
   
 
-  socket.on("connectEventMarket", async (data,fastify) => {
+  socket.on("connectEventMarket", async (data) => {
     const { commentaryId } = data;
     socket.join(commentaryId);
     const markets = await getMarketByComIdQuery({commentaryId : commentaryId}, fastify);
