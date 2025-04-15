c07161d9c6db0b14d0b4fec1b60e44e5c1b17949
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-04-15 13:47:38 +0400

    bgu fix

--- utilities/logger.js
+++ utilities/logger.js
@@ -375,6 +375,7 @@ const marektResultLogger = async (data, request, fastify) => {
 }
 const pythonSocketLogger = async (data, fastify) => {
   try {
+    let market = data.marketData?.map(JSON.parse);
     const query = `
       INSERT INTO "tblPythonSocketLogs"
       (
@@ -389,7 +390,7 @@ const pythonSocketLogger = async (data, fastify) => {
       type: fastify.db.QueryTypes.SELECT,
       bind: [
         data.commentaryId || null,
-        JSON.stringify(data.marketData) || null,
+        JSON.stringify(market) || null,
         data.socketId || null,
         data.createdAt || new Date(),
       ],
