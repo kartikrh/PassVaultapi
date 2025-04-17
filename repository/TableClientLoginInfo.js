1758f36f5bba931f0109f44edc8c104082328776
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-04-17 16:20:00 +0530

    clientLoginInfo changes

--- repository/TableClientLoginInfo.js
+++ repository/TableClientLoginInfo.js
@@ -1,28 +1,48 @@
 const { errorLogger } = require("../utilities/logger");
 
 const createClientLoginInfoQuery = async (data, fastify) => {
-  await fastify.db.query(
-    `with insert_data as (
-          INSERT INTO "tblClientLoginInfo" ("wrClientId", "wrInfo", "wrIsLogin", "wrCreatedAt") values ($1, $2, true, now()) 
-        )
-        update "tblClientLoginInfo" set "wrIsLogin" = false where "wrClientId" in (
-            select "wrClientId" from "tblClient" where "wrClientId" = $1 and "wrIsAllowMultiLogin" = false
-        )`,
-    {
+  try {
+    await fastify.db.query(
+      `with insert_data as (
+                  INSERT INTO "tblClientLoginInfo" ("wrClientId", "wrInfo", "wrIsLogin", "wrLoginType", "wrCreatedAt") values ($1, $2, $3, $4, now()) 
+                )
+                update "tblClientLoginInfo" set "wrIsLogin" = false where "wrClientId" in (
+                    select "wrClientId" from "tblClient" where "wrClientId" = $1 and "wrIsAllowMultiLogin" = false
+                )`,
+      {
         type: fastify.db.QueryTypes.SELECT,
-      bind: [data.clientId, JSON.stringify(data.info)],
-    }
-  );
+        bind: [data.clientId, JSON.stringify(data.info), data.isLogin, data.loginType],
+      }
+    );
+  } catch (error) {
+    errorLogger(
+      fastify,
+      error.message,
+      "DB ERROR --> repository/TableClientLoginInfo.js/createClientLoginInfoQuery",
+      null
+    );
+    throw new Error(error.message);
+  }
 };
 
 async function signOutClientInfoQuery(clientId, fastify) {
-  await fastify.db.query(
-    `UPDATE "tblClientLoginInfo" set "wrIsLogin" = false where "wrClientId" = $1`,
-    {
+  try {
+    await fastify.db.query(
+      `UPDATE "tblClientLoginInfo" set "wrIsLogin" = false where "wrClientId" = $1`,
+      {
         type: fastify.db.QueryTypes.SELECT,
-      bind: [clientId],
-    }
-  );
+        bind: [clientId],
+      }
+    );
+  } catch (error) {
+    errorLogger(
+      fastify,
+      error.message,
+      "DB ERROR --> repository/TableClientLoginInfo.js/signOutClientInfoQuery",
+      null
+    );
+    throw new Error(error.message);
+  }
 }
 
 module.exports = {
