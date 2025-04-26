7ad8cdccf80d81e5070b0ceb9ca8988fd763d038
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-04-26 19:40:48 +0530

    ball start api change

--- services/commentry.js
+++ services/commentry.js
@@ -4911,29 +4911,28 @@ const updateCommentaryStatusService = async (request, fastify) => {
     ...global.tblCommentaries[index],
     ...commentaryDetails,
   };
-    // call predictor endpoint
-  // if (global.tblCommentaries[index].isPredictMarket) {
-  //     callPredictorMarket(
-  //       {
-  //         commentary_id: commentaryId,
-  //         status: EventMarketStatus.Suspend,
-  //         match_type_id: global.tblCommentaries[index].matchTypeId,
-  //         is_open_market: false,
-  //         player_id : commentaryPlayerId || null
-  //       },
-  //       "/api/v1/updatemarketstatus",
-  //       fastify,
-  //       request
-  //     );
-  //     // let callPrediction = {};
-  //     // if (_resFromPredictAPI.data && _resFromPredictAPI.data.error_msg) {
-  //     //   callPrediction.predictioncallSuccess = false;
-  //     //   callPrediction.predictionMessage = _resFromPredictAPI.data.error_msg;
-  //     //   callPrediction.endPoint = '/api/v1/updatemarketstatus';
-  //     //   callPredictions.push(callPrediction);
-  //     //   callPrediction = {};
-  //     // }
-  // }
+  if (global.tblCommentaries[index].isPredictMarket) {
+      callPredictorMarket(
+        {
+          commentary_id: commentaryId,
+          status: EventMarketStatus.Suspend,
+          match_type_id: global.tblCommentaries[index].matchTypeId,
+          is_open_market: false,
+          player_id : commentaryPlayerId || null
+        },
+        "/api/v1/updatemarketstatus",
+        fastify,
+        request
+      );
+      // let callPrediction = {};
+      // if (_resFromPredictAPI.data && _resFromPredictAPI.data.error_msg) {
+      //   callPrediction.predictioncallSuccess = false;
+      //   callPrediction.predictionMessage = _resFromPredictAPI.data.error_msg;
+      //   callPrediction.endPoint = '/api/v1/updatemarketstatus';
+      //   callPredictions.push(callPrediction);
+      //   callPrediction = {};
+      // }
+  }
   
 
   if (
