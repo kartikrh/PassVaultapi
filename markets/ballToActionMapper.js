5775aa430701524085450ddb5b2090a459b92f7b
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2025-04-30 15:01:20 +0530

    Update ballToActionMapper.js

--- markets/ballToActionMapper.js
+++ markets/ballToActionMapper.js
@@ -296,7 +296,7 @@ function logBallToActionMapSample(commentaryId) {
     });
 
     // Write the data to ballToAction.json
-    fs.writeFileSync('ballToAction.json', JSON.stringify(ballToActionData, null, 2), 'utf8');
+    fs.writeFileSync(`ballToAction-${commentaryId}.json`, JSON.stringify(ballToActionData, null, 2), 'utf8');
 
     console.log("Ball-to-action map has been saved to 'ballToAction.json'");
 }
