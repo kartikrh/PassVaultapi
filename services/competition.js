dc2ae90c2bb875f349cb12db41330279b97e49cc
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-05-29 12:56:12 +0530

    fixed issue

--- services/competition.js
+++ services/competition.js
@@ -75,9 +75,6 @@ const allCompetitionService = async (request) => {
   if (isMen !== undefined) filterObject.isMen = isMen;
   if (type !== undefined && type !== 0) filterObject.type = type;
   if (typeof isVirtual === 'boolean') filterObject.isVirtual = isVirtual;
-  if (status !== undefined && status !== 0) filterObject.status = status;
-  if (startDate !== undefined && startDate !== 0) filterObject.startDate = startDate;
-  if (endDate !== undefined && endDate !== 0) filterObject.endDate = endDate;
 
   if (isActive === undefined || isTrending === undefined) {
     return global.tblCompetitions.filter((item) => item.isActive === true);
