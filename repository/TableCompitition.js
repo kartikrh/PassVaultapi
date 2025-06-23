8c60a1544e991717820b4c92900be0e884a65e13
Venkatesh Bandaru <venkateshb@Venkateshs-MacBook-Air.local>
2025-06-23 18:45:54 +0530

    change in comp

--- repository/TableCompitition.js
+++ repository/TableCompitition.js
@@ -190,6 +190,7 @@ const deleteCompetitionQuery = async (request, fastify) => {
 
 const updateCompititionQuery = async (data, fastify, request) => {
   try {
+    console.log("updateCompititionQuery", data)
     return await fastify.db.query(
       `
       update "tblCompetitions" set
@@ -245,7 +246,7 @@ const updateCompititionQuery = async (data, fastify, request) => {
           data.commStatus,
           data.startDate,
           data.endDate,
-          data.tpId,
+          data.tpId === undefined ? null : data.tpId,
           data.pythonId,
         ],
         type: fastify.db.QueryTypes.SELECT,
