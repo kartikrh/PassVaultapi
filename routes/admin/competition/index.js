db2d0f12fd8580d12bd4f906581334df8f790c7e
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-06-30 14:38:06 +0400

    change of permisiion

--- routes/admin/competition/index.js
+++ routes/admin/competition/index.js
@@ -158,7 +158,7 @@ module.exports = async (fastify, opts) => {
       (request, reply) =>
         checkPermission(request, reply, fastify, {
           tabName: "competition",
-          mode: "edit",
+          mode: "view",
         }),
     ],
     handler: (request, reply) => getAllMatchTypes(request, reply, fastify),
