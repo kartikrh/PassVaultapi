24934dbcd8099b325a113206ca851118a63d6c60
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-07-02 11:33:54 +0400

    bug fix

--- services/commentry.js
+++ services/commentry.js
@@ -19774,8 +19774,8 @@ const changePlayerService = async(request , fastify)=>{
     }
 
     let updatedData = await fastify.db.query(
-      `CALL proc_setcommentary(
-      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 ,$12,$13,$14 ,$15, $16, $17
+      `CALL proc_change_player(
+      $1, $2, $3, $4, $5, $6
     )`,
       {
         bind: [
