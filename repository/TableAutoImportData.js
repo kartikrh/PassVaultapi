ff721783bc1233c2bc7d811b4e13d270186f306f
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-08-06 12:47:02 +0530

    new apis on autoImportData

--- repository/TableAutoImportData.js
+++ repository/TableAutoImportData.js
@@ -1,4 +1,5 @@
 const { errorLogger } = require("../utilities/logger");
+const { getPagination } = require("../utilities");
 
 const getAllAutoImportDataQuery = async (request, fastify) => {
     try {
@@ -160,10 +161,94 @@ const updateAutoImportDataQuery = async (data, fastify, request) => {
         throw new Error(err.message);
     }
 };
+const deleteAutoImportDataQuery = async (id, request, fastify) => {
+  try {
+    return await fastify.db.query(
+      `
+           DELETE FROM "tblAutoImportData"
+           WHERE "wrId" = ANY ($1)
+            `,
+      {
+        type: fastify.db.QueryTypes.DELETE,
+        bind: [id],
+      }
+    );
+  } catch (err) {
+    errorLogger(
+      fastify,
+      err.message,
+      "DB ERROR --> repository/TableAutoImportData/deleteAutoImportDataQuery",
+      request
+    );
+    throw new Error(err.message);
+  }
+};
+
+const allAutoImportDataLogsQuery = async (body ,request, fastify) => {
+    try {
+        const { startDate, endDate, page = 1, limit = 20 } = body;
+        const {skip , take} = getPagination(page, limit);
+        const where = startDate && endDate ? `WHERE "wrCreateDate" BETWEEN '${startDate}' AND '${endDate}'` : '';
+        const query = `
+            SELECT 
+                "wrId" as "id",
+                "wrRefId" as "refId",
+                "wrRefType" as "refType",
+                "wrSourceId" as "sourceId",
+                "wrIsImported" as "isImported",
+                "wrIsImportStart" as "isImportStart",
+                "wrImportStartTime" as "importStartTime",
+                "wrImportEndTime" as "importEndTime",
+                "wrCreatedBy" as "createdBy",
+                "wrCreateDate" as "createdDate"
+            FROM "tblAutoImportData"
+            ${where}
+            ORDER BY "wrId" DESC
+            LIMIT $1 OFFSET $2;
+        `;
+        const data = await fastify.db.query(query, {
+            type: fastify.db.QueryTypes.SELECT,
+            bind : [
+                take,
+                skip
+            ]
+        }); 
+
+        const totalRecordsQuery = `
+            SELECT COUNT(*) as "count"
+            FROM "tblAutoImportData"
+            ${where}
+        `;
+
+        const totalRecordsResult = await fastify.db.query(totalRecordsQuery, {
+            type: fastify.db.QueryTypes.SELECT,
+        });
+
+        const totalRecords = parseInt(totalRecordsResult[0].count, 10);
+        const totalPages = Math.ceil(totalRecords / take);
+
+        return {
+            totalRecords: totalRecords,
+            currentPage: page,
+            totalPages: totalPages,
+            data: data,
+        };
+    } catch (err) {
+        errorLogger(
+            fastify,
+            err.message,
+            "DB ERROR --> repository/TableAutoImportData.js/allAutoImportDataLogsQuery",
+            request
+        );
+        throw new Error(err.message);
+    }
+};
 
 module.exports = {
     getAllAutoImportDataQuery,
     getAutoImportDataByIdQuery,
     insertAutoImportDataQuery,
     updateAutoImportDataQuery,
+    deleteAutoImportDataQuery,
+    allAutoImportDataLogsQuery,
 };
