573cf1d3420d7a6edd90699cc6ead6969d930d40
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-09-12 11:57:23 +0530

    delete teamPlayer validation changes

--- services/agent.js
+++ services/agent.js
@@ -471,7 +471,8 @@ async function getAgentWisePermissionService(request, fastify) {
 
 const getInitConfigDetails = async (request, fastify) => {
     const initKeys = [configConstants.DPAPIURL, configConstants.DPAPIXKEY, configConstants.DPSOCKETURL, configConstants.SCORECARDFRAMEURL, configConstants.ENABLELOGROCKET, configConstants.LOGROCKETAPPID,
-    configConstants.ISAPPLYPLAYERSTRIKELOGIC, configConstants.ISAPPLYPARTNERSHIPLOGIC, configConstants.ENTITYSPORTURL];
+      configConstants.ISAPPLYPLAYERSTRIKELOGIC, configConstants.ISAPPLYPARTNERSHIPLOGIC, configConstants.ENTITYSPORTURL,
+      configConstants.STREAMINGURL, configConstants.STREAMINGXKEY, configConstants.STREAMINGWATCHURL];
     let result = global.tblConfigs.filter(item => initKeys.includes(item.key));
     return result;
 }
