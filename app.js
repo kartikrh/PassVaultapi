2d144fb2007eb5cf06440ab431a3420438458cf0
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-09-27 13:13:09 +0530

    change

--- app.js
+++ app.js
@@ -458,7 +458,7 @@ module.exports = async function (fastify, opts) {
         "https://admin.socket.io",
         "https://panel.deployed.live",
         "http://localhost:3001",
-        "https://uatpanel.deployed.live",
+        // "https://uatpanel.deployed.live",
         "https://uatpanel.scorre.info",
         "http://localhost:8080"
       ],
