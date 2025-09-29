050de9f69c0d55bdabafacd7f6ad6aa5143fe3e4
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-09-29 16:13:49 +0530

    change of domain

--- app.js
+++ app.js
@@ -459,7 +459,7 @@ module.exports = async function (fastify, opts) {
         "https://panel.deployed.live",
         "http://localhost:3001",
         "https://uatpanel.deployed.live",
-        // "https://uatpanel.scorre.info",
+        "https://uatpanel.scorre.info",
         "http://localhost:8080"
       ],
       credentials: true,
