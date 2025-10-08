a55c8350667f84225d9654b068e861902be20dc7
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-10-08 10:14:09 +0530

    teamPoint and saveDevice api changes

--- services/score.js
+++ services/score.js
@@ -20,6 +20,7 @@ const {
 } = require("../repository/TableCommentary");
 const { dltDeviceQuery, saveDeviceQuery } = require("../repository/TableDevice");
 const { weatherAndPitchDataService } = require("../services/commentry");
+const { getIdByValue } = require("../repository/TableUser");
 
 // const getAllCommentariesDataService = async (request,fastify) => {
 //     try {
@@ -921,7 +922,11 @@ const getMarketsByCommentaryIdServiceV1 =async (request , fastify) => {
 const saveDeviceDataService = async (request, fastify) => {
     const devices = global.tblDevices || [];
     // go accroding to device type and then userId at the time only one data will be saved with one device type and userId or devictype and tempCId if not logged in
-    const { deviceType, userId, tempCId } = request.body;
+    let { deviceType, userId, tempCId } = request.body;
+    if (userId && typeof userId === "string") {
+      let checkExist = await getIdByValue({ clientId: userId }, request, fastify);
+      userId = checkExist?.clientId ?? 0;
+    }
     let deviceData = devices.filter((d) => d.deviceType === deviceType && (d.userId === userId || d.tempCId === tempCId));
     if(deviceData.length > 0){
         // delete the old device data
@@ -934,7 +939,7 @@ const saveDeviceDataService = async (request, fastify) => {
         global.tblDevices = global.tblDevices.filter((d) => !ids.includes(d.deviceId));
         // save the new device data
         let dData = await saveDeviceQuery(
-            request.body,
+            { ...request.body, userId },
             fastify,
             request
         );
@@ -944,7 +949,7 @@ const saveDeviceDataService = async (request, fastify) => {
     else {
         // save the new device data
         let dData = await saveDeviceQuery(
-            request.body,
+            { ...request.body, userId },
             fastify,
             request
         );
