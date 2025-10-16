804cc9597185384fb39978993f2b66dc06732c5f
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-10-16 11:24:35 +0530

    bug fix

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ module.exports = async function (fastify, opts) {
         "iccRankingModel"
       ];
       
-      models.forEach((model) => require(`./sequelize/tables/${model}`)(fastify.db,fastify.db.Sequelize));
+      models.forEach((model) => require(`./sequelize/tables/${model}`)(fastify.db));
       setImmediate(async () => {
         try {
           // await featchData(fastify);
