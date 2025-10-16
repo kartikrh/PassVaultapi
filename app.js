abfc46dd58fd84ec9a8dbf588d1e360ecc1509d6
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-10-16 11:46:19 +0530

    bug fix

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ module.exports = async function (fastify, opts) {
     .register(fsequelize, {
       ...dbPg,
       instance: "db", // tells the plugin to create a Sequelize instance with the name "db"
-      // models: path.join(__dirname, "sequelize/tables"), // <-- point to folder
+      models: path.join(__dirname, "sequelize", "tables", "userModel.js"),
     })
     .after(async () => {
       // Load models and sync DB
