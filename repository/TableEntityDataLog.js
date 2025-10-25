2e06ba866dc37075c250b62bcef3d00e4f278d96
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-10-25 12:36:42 +0530

    console changes

--- repository/TableEntityDataLog.js
+++ repository/TableEntityDataLog.js
@@ -2,6 +2,7 @@ const { errorLogger } = require("../utilities/logger");
 
 const createDataQuery = async (data  , fastify)=>{
     try {
+        console.log("entiy data log data", data)
         let result = await fastify.db.query(`
                 INSERT INTO public."tblEntityDataLogs"(
                  "wrData", "wrMatchId")
