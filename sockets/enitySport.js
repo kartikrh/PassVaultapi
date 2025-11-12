fce5e9b9e9289f08c35a84dea8f30ddd7238191e
venkateshbandaruwpa <venkatesh@ziwotech.com>
2025-11-12 12:09:29 +0530

    change

--- sockets/enitySport.js
+++ sockets/enitySport.js
@@ -19,6 +19,7 @@ function addToQueue(payload, fastify) {
   // Replace existing queued item if same matchId (avoid duplicates)
   commentaryQueue.set(matchId, { payload, fastify });
   processTimeout = setTimeout(() => {
+    console.log("addToQueue----")
     if (!isProcessingQueue) processQueue();
   }, 100);
 
