5fb6e4922f81188f29f477db351fe5bd04d2605b
Jyoti-wpa <jyothi.wpa@gmail.com>
2025-11-19 11:06:21 +0530

    add log

--- sockets/index.js
+++ sockets/index.js
@@ -72,6 +72,7 @@ const connectClients = async (fastify, clientSocketId = undefined) => {
                   if (elem.commentaryId) {
                     const index = global.tblCommentaries.findIndex(i => i.commentaryId == elem.commentaryId);
                     if(index == -1){
+                      console.log("elem", elem)
                       errorLogger(
                         fastify,
                         "CommentaryId not found in global  socketObj.cronJob",
