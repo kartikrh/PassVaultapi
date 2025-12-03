2320ed775870015feb907ea55b8399188dec7372
geektechie <geektechie712@gmail.com>
2025-12-03 16:07:37 +0530

    add entityEnum filter in competition statistics type api

--- services/competitionStatisticsType.js
+++ services/competitionStatisticsType.js
@@ -8,16 +8,16 @@ const getAllCompetitionStatisticsTypeService = async (request, fastify) => {
         competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.isActive === isActive);
     }
 
-    if (typeId && typeId !== 0) {
-        competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.typeId === typeId);
-    }
-
     if (eventTypeId && eventTypeId !== 0) {
         competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.eventTypeId === eventTypeId);
     }
 
-    if (entityEnum && entityEnum !== 0) {
-        competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.entityEnum === entityEnum);
+    if (typeId && typeId !== 0) {
+        competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.typeId === typeId);
+
+        if (entityEnum && entityEnum !== 0) {
+            competitionStatisticsTypes = competitionStatisticsTypes.filter(item => item.entityEnum === entityEnum);
+        }
     }
 
     return competitionStatisticsTypes;
@@ -122,12 +122,12 @@ const updateCompetitionStatisticsTypeDisplayOrderService = async (request, fasti
     for (const item of request.body) {
         await updateCompetitionStatisticsTypeDisplayOrderQuery(item, request, fastify);
         let index = global.tblCompetitionStatisticsType.findIndex((elem) => elem.id === item.competitionStatisticsTypeId);
-        if(index !== -1){
+        if (index !== -1) {
             global.tblCompetitionStatisticsType[index].displayOrder = item.displayOrder;
         }
-      }
-    
-      return `Display order updated successfully`;
+    }
+
+    return `Display order updated successfully`;
 }
 
 module.exports = {
