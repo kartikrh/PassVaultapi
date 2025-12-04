e4e25d772b1f341a0a080a47d29e6b284fbf1a0d
ApurvaKhurudWpa <apoorva.wpa@gmail.com>
2025-12-04 12:49:54 +0530

    ui changes for jerseyPlayerImage in ranking listing

--- repository/TablePlayer.js
+++ repository/TablePlayer.js
@@ -716,6 +716,7 @@ const getAllDuplicatePlayersQuery = async (request, fastify) => {
       //     type: fastify.db.QueryTypes.SELECT,
       //   }
       // );
+
       let res = await fastify.db.query(
         `
             CALL proc_get_duplicate_data($1)
