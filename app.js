c713df0c7db25f59aadfea0fcd699bad88996c2a
geektechie <geektechie712@gmail.com>
2025-12-09 15:36:35 +0530

    revert changes

--- app.js
+++ app.js
@@ -168,8 +168,6 @@ module.exports = async function (fastify, opts) {
         }
       } catch (error) {
         console.error("Error during scheduled task - entitySportAutoImportProcess:", error);
-      } finally {
-        isAutoImportProcessEnded = true;
       }
     });
 
