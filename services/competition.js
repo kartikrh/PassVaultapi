7e76f13fa55f5562eae85be5ed2278713de3ae02
geektechie <geektechie712@gmail.com>
2025-12-11 15:14:43 +0530

    change points in competition if table 1

--- services/competition.js
+++ services/competition.js
@@ -1332,7 +1332,7 @@ const competitionImportService = async (data, fastify, request) => {
 
   let checkCompetition = global.tblCompetitions.find(item => item.tpId === data.cid);
   if (!checkCompetition) {
-    const competitionData = {
+    let competitionData = {
       competition: entitySportCompetitionResponse?.title,
       eventTypeId: eventType?.eventTypeId || EventType["Cricket"],
       refId: entitySportCompetitionResponse?.cid,
@@ -1347,8 +1347,18 @@ const competitionImportService = async (data, fastify, request) => {
       endDate: entitySportCompetitionResponse?.dateend,
       tpId: entitySportCompetitionResponse?.cid,
       pythonId: pythonIdData?.id || null,
-      isPointTable: entitySportCompetitionResponse?.table === "1"
+      isPointTable: entitySportCompetitionResponse?.table === "1",
     };
+
+    if (competitionData.isPointTable) {
+      competitionData = {
+        ...competitionData,
+        winPoint: 2,
+        tiePoint: 0,
+        lossPoint: 0,
+        cancelPoint: 1,
+      }
+    }
 
     const insertCompetition = await insertCompetitionQuery({
       ...request,
