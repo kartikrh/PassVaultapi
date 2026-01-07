415b32a699a53a7706f033099964fd67e88b888e
venkateshbandaruwpa <venkatesh@ziwotech.com>
2026-01-07 16:42:03 +0530

    wicketType keys

--- utilities/index.js
+++ utilities/index.js
@@ -2082,17 +2082,29 @@ const CompetitionStatisticsType = {
     }
   }
 };
+// const etWicketObj = {
+//   "caught" : wicketTypeObj.CATCH,
+//   "bowled" : wicketTypeObj.BOLD,
+//   "lbw" : wicketTypeObj.LBW,
+//   "stumped" : wicketTypeObj.STUMP,
+//   "run out" : wicketTypeObj.RUN_OUT,
+//   "hit wicket" : wicketTypeObj.HIT_WICKET,
+//   "retired hurt" : wicketTypeObj.RETIRED_OUT,
+//   "timed out" : wicketTypeObj.TIMED_OUT,
+//   "hit the ball twice" : wicketTypeObj.HIT_BALL_TWICE,
+//   "obstructing the field" : wicketTypeObj.OBSTRACT_THE_FIELDING,
+// }
 const etWicketObj = {
   "caught" : wicketTypeObj.CATCH,
   "bowled" : wicketTypeObj.BOLD,
   "lbw" : wicketTypeObj.LBW,
   "stumped" : wicketTypeObj.STUMP,
   "runout" : wicketTypeObj.RUN_OUT,
-  "hit wicket" : wicketTypeObj.HIT_WICKET,
-  "retired hurt" : wicketTypeObj.RETIRED_OUT,
-  "timed out" : wicketTypeObj.TIMED_OUT,
-  "hit the ball twice" : wicketTypeObj.HIT_BALL_TWICE,
-  "obstructing the field" : wicketTypeObj.OBSTRACT_THE_FIELDING,
+  "hitwicket" : wicketTypeObj.HIT_WICKET,
+  "retiredout" : wicketTypeObj.RETIRED_OUT,
+  "timedout" : wicketTypeObj.TIMED_OUT,
+  "hittheballtwice" : wicketTypeObj.HIT_BALL_TWICE,
+  "fieldobstruction" : wicketTypeObj.OBSTRACT_THE_FIELDING,
 }
 
 const getKeyAndValueKey = async (enumValue) => {
