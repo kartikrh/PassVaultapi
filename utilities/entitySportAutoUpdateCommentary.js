64e9b6e6761fa8c77a44ad5cc719b44f61c5b9d6
geektechie <geektechie712@gmail.com>
2026-01-23 19:18:15 +0530

    fix null issue in date

--- utilities/entitySportAutoUpdateCommentary.js
+++ utilities/entitySportAutoUpdateCommentary.js
@@ -447,7 +447,6 @@ const entitySportAutoUpdateCommentary = async (fastify) => {
                                     });
                                 }
 
-                                console.log("🚀 ~ entitySportAutoUpdateCommentary ~ insertAutoUpdateCommentaryData:", insertAutoUpdateCommentaryData)
                                 if (insertAutoUpdateCommentaryData?.id) {
                                     await updateAutoUpdateCommentaryDataQuery({
                                         status: isChanged ? autoUpdateCommentaryDataStatus.success : autoUpdateCommentaryDataStatus.noupdate,
