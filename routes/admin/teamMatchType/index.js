edb78ddf8fbfa842e103e0691667bbf44bcb67af
geektechie <geektechie712@gmail.com>
2026-02-09 19:15:47 +0530

    add active/inactive and delete team match type api

--- routes/admin/teamMatchType/index.js
+++ routes/admin/teamMatchType/index.js
@@ -1,5 +1,5 @@
 const { authorize, checkPermission } = require("../../../controller/middleware");
-const { getTeamMatchTypeByTeamId, saveTeamMatchTypeByTeamId, updateTeamMatchDataTypeByTeamId } = require("../../../controller/users/admin/teamMatchType");
+const { getTeamMatchTypeByTeamId, saveTeamMatchTypeByTeamId, updateTeamMatchDataTypeByTeamId, activeInactiveTeamMatchDataTypeByTeamId, deleteTeamMatchTypeByTeamId } = require("../../../controller/users/admin/teamMatchType");
 const { TeamMatchType } = require("../../../swaggerSchema/groupTags/schema");
 
 module.exports = async (fastify, opts) => {
@@ -38,5 +38,29 @@ module.exports = async (fastify, opts) => {
                 }),
         ],
         handler: (request, reply) => updateTeamMatchDataTypeByTeamId(request, reply, fastify),
+    });
+    fastify.post("/activeInactive", {
+        schema: TeamMatchType.activeInactive.schema,
+        preHandler: [
+            (request, reply) => authorize(request, reply, fastify),
+            (request, reply, done) =>
+                checkPermission(request, reply, fastify, {
+                    tabName: "Teams",
+                    mode: "edit",
+                }),
+        ],
+        handler: (request, reply) => activeInactiveTeamMatchDataTypeByTeamId(request, reply, fastify),
+    });
+    fastify.post("/delete", {
+        schema: TeamMatchType.delete.schema,
+        preHandler: [
+            (request, reply) => authorize(request, reply, fastify),
+            (request, reply, done) =>
+                checkPermission(request, reply, fastify, {
+                    tabName: "Teams",
+                    mode: "delete",
+                }),
+        ],
+        handler: (request, reply) => deleteTeamMatchTypeByTeamId(request, reply, fastify),
     });
 }
