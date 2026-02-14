89a3c5aa3bd7f84c77d576d996b5b412c8270396
geektechie <geektechie712@gmail.com>
2026-02-14 09:44:37 +0530

    fix match type id null issue

--- services/competition.js
+++ services/competition.js
@@ -1470,7 +1470,7 @@ const upsertCommentaryTeamsAndPlayersService = async (checkCompetition, tourname
       ...request,
       body: {
         teamId: team.teamId,
-        matchTypeId: matchTypeId
+        matchTypeId: -1
       }
     }, fastify);
 
