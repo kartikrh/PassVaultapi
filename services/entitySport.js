e9b664c74ab9e5dc3095beb6995928cbcf2dd104
Jyoti-wpa <jyothi.wpa@gmail.com>
2026-02-18 16:12:28 +0400

    remove wrong code

--- services/entitySport.js
+++ services/entitySport.js
@@ -9,7 +9,7 @@ const {
   updateBallByBallFullCommentaryQuery,
 } = require("../repository/TableCommentary")
 const { getAllTournamentTeamPlayerByIdsQuery } = require("../repository/TableTournamentsTeamPlayers")
-const { getMatchDataByCId, syncEntitySportCommentaryService, updateCommentaryPlayersFromEntityService ,addSuperOverInEntity} = require("../services/commentry");
+const { getMatchDataByCId, syncEntitySportCommentaryService, updateCommentaryPlayersFromEntityService} = require("../services/commentry");
 const {
     callClientAPI,
     ServiceType,
@@ -937,22 +937,8 @@ const setEntityCom2Service = async (request , fastify) =>{
         // }
         if(comDetails.commentaryStatus == commentaryStatus.INPROGRESS){
           comDetails.isClientShow = true;
-          // check for super over
-          let isSuperOver = response.live.live_inning.issuperover || "false";
-          if(isSuperOver == "true"){
-            // set super ove first
-            let latestInning = response.live.live_inning_number;
-            let alExist = global.tblCommentaryTeams.find((i)=> i.commentaryId == comDetails.commentaryId && i.subInning == latestInning);
-            if(!alExist){
-              await addSuperOverInEntity(request.body , request,fastify , comDetails)
-              comDetails = global.tblCommentaries.find((i)=>i.commentaryId == comDetails.commentaryId)
-            }
-          }
-          if(response.live.commentaries && response.live.commentaries.length > 0){
-            let res = await handleComArr(request.body, request,fastify,comDetails)
-            return res;
-          }
-          return true;
+          let res = await handleComArr(request.body, request,fastify,comDetails)
+          return res;
         }
         if(comDetails.commentaryStatus == commentaryStatus.INNINGCHANGE){
           comDetails.isClientShow = false;
