423a89b9f045c27a6fd9241242b4ede18de1ca8d
Jyoti-wpa <jyothi.wpa@gmail.com>
2026-02-18 14:18:25 +0400

    bug fix

--- services/entitySport.js
+++ services/entitySport.js
@@ -3,7 +3,7 @@ const { getTeamsByIds } = require("../repository/TableTeams")
 const { getCompetitionByIdsQuery } = require("../repository/TableCompitition")
 const { getComEntityQuery, updateVirtualPartnershipQuery, updateCommentaryStatusQuery, scoringTypeCommentaryQuery } = require("../repository/TableCommentary")
 const { getAllTournamentTeamPlayerByIdsQuery } = require("../repository/TableTournamentsTeamPlayers")
-const { getMatchDataByCId, syncEntitySportCommentaryService, updateCommentaryPlayersFromEntityService } = require("../services/commentry");
+const { getMatchDataByCId, syncEntitySportCommentaryService, updateCommentaryPlayersFromEntityService ,addSuperOverInEntity} = require("../services/commentry");
 const {
     callClientAPI,
     ServiceType,
@@ -931,8 +931,22 @@ const setEntityCom2Service = async (request , fastify) =>{
         // }
         if(comDetails.commentaryStatus == commentaryStatus.INPROGRESS){
           comDetails.isClientShow = true;
-          let res = await handleComArr(request.body, request,fastify,comDetails)
-          return res;
+          // check for super over
+          let isSuperOver = response.live.live_inning.issuperover || "false";
+          if(isSuperOver == "true"){
+            // set super ove first
+            let latestInning = response.live.live_inning_number;
+            let alExist = global.tblCommentaryTeams.find((i)=> i.commentaryId == comDetails.commentaryId && i.subInning == latestInning);
+            if(!alExist){
+              await addSuperOverInEntity(request.body , request,fastify , comDetails)
+              comDetails = global.tblCommentaries.find((i)=>i.commentaryId == comDetails.commentaryId)
+            }
+          }
+          if(response.live.commentaries && response.live.commentaries.length > 0){
+            let res = await handleComArr(request.body, request,fastify,comDetails)
+            return res;
+          }
+          return true;
         }
         if(comDetails.commentaryStatus == commentaryStatus.INNINGCHANGE){
           comDetails.isClientShow = false;
@@ -2850,7 +2864,7 @@ const multiInningChangeService = async (data, fastify, comDetails) => {
         ...partnership,
         isActive: false,
       };
-      await upActivePartQuery(part, fastify);
+      await upActivePartQuery(partnership, fastify);
       let partIndex = global.tblCommentaryPartnership.findIndex(
         (item) => item.commentaryPartnershipId == part.commentaryPartnershipId
       );
