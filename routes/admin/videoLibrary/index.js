6695620d5da99926df09b481a038957083e3468f
venkateshbandaruwpa <venkatesh@ziwotech.com>
2026-04-22 12:06:18 +0530

    endpoin changes

--- routes/admin/videoLibrary/index.js
+++ routes/admin/videoLibrary/index.js
@@ -38,7 +38,7 @@ module.exports = async (fastify, opts) => {
     handler: (request, reply) => updateVideoStatus(request, reply, fastify)
   });
 
-  fastify.post("/updateDisplayOrder", {
+  fastify.post("/changeDisplayOrder", {
     preHandler: [(request, reply) => authorize(request, reply, fastify)],
     handler: (request, reply) => updateDisplayOrder(request, reply, fastify)
   });
