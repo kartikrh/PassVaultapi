1b5602e666ec4ac47554c6dd8d4d966cf0abbd3d
Jyoti-wpa <jyothi.wpa@gmail.com>
2026-04-24 13:20:08 +0400

    chang ein schema

--- swaggerSchema/groupTags/schema.js
+++ swaggerSchema/groupTags/schema.js
@@ -9441,8 +9441,8 @@ const PhotoLibrary = {
           SEO: { type: "string" },
           description: { type: "string" },
           isPermanent: { type: "boolean" },
-          startDate: { type: "string", format: "date-time", nullable: true },
-          endDate: { type: "string", format: "date-time", nullable: true }
+          startDate: { type: "string", nullable: true },
+          endDate: { type: "string", nullable: true }
         },
         required: ["photoLibraryId", "title"],
       },
