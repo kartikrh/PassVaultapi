40f955ad5e2b04778137a876dd3bd214705af047
geektechie <geektechie712@gmail.com>
2026-04-25 18:44:44 +0530

    worked on remove call client api on save like/dislike

--- services/clientLikeDislikeActivity.js
+++ services/clientLikeDislikeActivity.js
@@ -1,6 +1,4 @@
 const { getByClientTypeRefIdQuery, insertClientLikeDislikeActivityQuery, updateClientLikeDislikeActivityQuery, getlikeDislikeByTypeRefIdQuery } = require("../repository/TableClientLikeDislikeActivity");
-const { ServiceType, APIEndpointModuleType, callClientAPI } = require("../utilities");
-const { errorLogger } = require("../utilities/logger");
 
 const getByClientTypeRefIdService = async (request, fastify) => {
     return await getByClientTypeRefIdQuery(request, fastify);
@@ -24,6 +22,7 @@ const saveClientLikeDislikeActivityService = async (request, fastify) => {
     }
     result = result?.[0];
 
+    let response = null;
     if (result?.type === 1) {
         const index = global.tblVideoLibrary.findIndex(item => item.id === result.refId);
         if (index !== -1) {
@@ -36,31 +35,15 @@ const saveClientLikeDislikeActivityService = async (request, fastify) => {
             }, fastify);
             global.tblVideoLibrary[index].likeCount = getLatest?.[0].likeCount || 0;
             global.tblVideoLibrary[index].dislikeCount = getLatest?.[0].dislikeCount || 0;
-            result = global.tblVideoLibrary[index];
+            response = {
+                ...global.tblVideoLibrary[index],
+                likeDislikeType: 1
+            };
             global.pendingVideoLibraryToClient = global.pendingVideoLibraryToClient.filter(item => item.id !== global.tblVideoLibrary[index].id);
-
-            callClientAPI(
-                {
-                    serviceType: ServiceType.clientAPI,
-                    moduleType: APIEndpointModuleType.updateSeoModule,
-                    data: {
-                        module: 'videoLibrary',
-                        type: "update",
-                        data: global.tblVideoLibrary[index]
-                    }
-                }, request, fastify)
-                .catch((err) => {
-                    errorLogger(
-                        fastify,
-                        err.message,
-                        "services/clientLikeDislikeActivity.js/saveClientLikeDislikeActivityService - callClientAPI",
-                        request
-                    );
-                });
         }
     }
 
-    return result;
+    return response;
 };
 
 module.exports = {
