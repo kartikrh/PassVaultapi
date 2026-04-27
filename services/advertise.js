c8591f73277effcbce3638093f1166979573f189
venkateshbandaruwpa <venkatesh@ziwotech.com>
2026-04-27 14:29:58 +0530

    news and advertise client changes

--- services/advertise.js
+++ services/advertise.js
@@ -357,9 +357,30 @@ const changeDisplayOrderService = async (request, fastify) => {
       global.tblAdvertise[index].displayOrder = item.displayOrder;
     }
   }
-  let allActiveData = global.tblAdvertise.filter(
-    (item) => item.isActive == true
+  const now = Date.now();
+  let allActiveData = global.tblAdvertise.filter(item =>
+    item.isActive === true && (item.isPermanent === true || (item.startDate <= now && item.endDate >= now))
   );
+  callClientAPI(
+    {
+      serviceType: ServiceType.clientAPI,
+      moduleType: APIEndpointModuleType.upsertAdvertiseDataToClient,
+      data: {
+        type: ClientAPIType.ChangeDisplayOrder,
+        advertise: allActiveData
+      }
+    },
+    request,
+    fastify
+  ).catch((err) => {
+    console.log("call client api console", err);
+    errorLogger(
+      fastify,
+      err.message,
+      "ERROR --> services/advertise.js/changeDisplayOrderService - callClientAPI",
+      request
+    );
+  });
  
   return true;
 };
