2425228d3e06a704561b8583d2087720ef0c0a3c
geektechie <geektechie712@gmail.com>
2026-04-27 16:56:22 +0530

    remove temp log

--- services/clientLikeDislikeActivity.js
+++ services/clientLikeDislikeActivity.js
@@ -4,14 +4,13 @@ const { getOriginalIdFromEncryptedId } = require("../repository/TableUser");
 const checkClientService = async (request, fastify) => {
     const clientId = request.body.clientId;
     let getClientId = await getOriginalIdFromEncryptedId(clientId, fastify);
-    console.log("🚀 ~ checkClientService ~ getClientId:", getClientId)
     if (!getClientId) {
         throw new Error(`Client with this encrypted id ${clientId} not found!`);
     }
 
-    const checkExist = global.tblClient.find(item => item.clientId === getClientId.wrKey && item.isActive === true);
+    const checkExist = global.tblClient.find(item => item.clientId === getClientId && item.isActive === true);
     if (!checkExist) {
-        throw new Error(`Client with this id ${getClientId.wrKey} not found!`);
+        throw new Error(`Client with this id ${getClientId} not found!`);
     }
 
     return checkExist;
