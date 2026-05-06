0a901f258c1ed1e7e35cac277d2caf05e32b774f
geektechie <geektechie712@gmail.com>
2026-05-06 15:20:48 +0530

    add temp log

--- services/entitySport.js
+++ services/entitySport.js
@@ -373,7 +373,7 @@ const setEntityCom2Service = async (request , fastify) =>{
     const eventDate = new Date(response.match_info.date_start.replace(" ", "T") + "Z");
     const currentDate = new Date();
     const diffHours = (currentDate - eventDate) / (1000 * 60 * 60);
-    console.log("🚀 ~ setEntityCom2Service ~ dateTime:", eventDate, currentDate, diffHours)
+    console.log("🚀 ~ setEntityCom2Service ~ dateTime:", matchID, response.match_info.date_start, eventDate, currentDate, diffHours)
     if (diffHours > 48) {
       let commentaryData = global.tblCommentaries.find(tc => tc.tpId === response.match_id);
       const checkCompetition = global.tblCompetitions.find(item => item.tpId === response.match_info?.competition?.cid);
