0b372fb69a2e3c535c94a0905a4baea8f004672a
geektechie <geektechie712@gmail.com>
2026-05-08 10:07:47 +0530

    add null in prevgroupid schema

--- swaggerSchema/groupTags/schema.js
+++ swaggerSchema/groupTags/schema.js
@@ -8915,7 +8915,7 @@ const TournamentTeamPoints = {
             totalPoint: { type: "integer" },
             isActive: { type: "boolean" },
             id: { type: "integer" },
-            prevGroupId: { type: "integer" }
+            prevGroupId: { type: ["integer", "null"] }
           },
           required: ["groupId", "competitionId", "teamId", "id"],
       },
