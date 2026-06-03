cdaeddcdb46e20db874e43998c9c19697983e195
geektechie <geektechie712@gmail.com>
2026-06-03 11:57:09 +0530

    remove temp logs

--- repository/TableCompetitionStatistics.js
+++ repository/TableCompetitionStatistics.js
@@ -252,7 +252,6 @@ const updateCompetitionStatisticsByIdQuery = async (data, fastify, request) => {
 };
 
 const deleteCompetitionStatisticsByIdQuery = async (competitionStatisticsId, fastify, request) => {
-    console.log("🚀 ~ deleteCompetitionStatisticsByIdQuery ~ competitionStatisticsId:", competitionStatisticsId)
     try {
         await fastify.db.query(
             `
