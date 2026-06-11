05280ce6a7b40e975c20767754681ff90f5a73a0
geektechie <geektechie712@gmail.com>
2026-06-11 14:29:58 +0530

    add registration process filter

--- services/client.js
+++ services/client.js
@@ -2,7 +2,7 @@ const { deleteClientQuery, insertClientQuery, updateClientQuery, activeInactiveC
 const { encrypt, decrypt } = require("../utilities/index");
 
 const getAllClientService = async (request, fastify) => {
-  const { isActive, isUserActive, provider } = request.body;
+  const { isActive, isUserActive, provider, registrationProcessStatus } = request.body;
   let result = global.tblClient.map(({ password, seamlessToken, ...res }) => res);
   if (isActive == undefined) {
     result = result;
@@ -18,6 +18,10 @@ const getAllClientService = async (request, fastify) => {
 
   if (provider != null || provider != undefined) {
     result = result.filter((item) => item.provider == provider);
+  }
+
+  if (registrationProcessStatus != null || registrationProcessStatus != undefined) {
+    result = result.filter((item) => item.registrationProcessStatus == registrationProcessStatus);
   }
 
   return result
