32d50ee042fd9df364141c8e6f806eb555ff2575
geektechie <geektechie712@gmail.com>
2026-07-28 16:58:15 +0530

    add temp log

--- sockets/client.js
+++ sockets/client.js
@@ -60,6 +60,7 @@ const connectClients = async (fastify, clientSocketId) => {
           null
         );
 
+        console.log("🚀 ~ connectClients ~ config:", config)
         client.emit("isAPNSEnable", config?.isAPNSEnable ? config : null);
 
         try {
