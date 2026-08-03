12c8d12f5458172886516c9f5d709b01740c7a44
geektechie <geektechie712@gmail.com>
2026-08-03 19:50:17 +0530

    update schema of get all activity token and and logs

--- swaggerSchema/groupTags/schema.js
+++ swaggerSchema/groupTags/schema.js
@@ -11948,21 +11948,47 @@ const ClientLikeDislikeActivity = {
 }
 
 const LiveActivityToken = {
-  getAll :{
-    schema : {
-      tags : ["LiveActivityToken"],
-      description : "get all APNS Live Activity Token Data",
-      secaurity : [{bearerAuth : []}]
+  getAll: {
+    schema: {
+      tags: ["LiveActivityToken"],
+      description: "get all APNS Live Activity Token Data",
+      secaurity: [{ bearerAuth: [] }],
+      body: {
+        type: "object",
+        properties: {
+          isActive: { type: "boolean" },
+          limit: { type: "integer" },
+          page: { type: "string" },
+          startDate: { type: "string" },
+          endDate: { type: "string" },
+          envType: { type: "integer" },
+          clientSocketId: { type: "integer" }
+        },
+        required: ["isActive", "limit", "page"]
+      }
     }
   }
 }
 
 const APNSLiveActivityLogs = {
-  getAll :{
-    schema : {
-      tags : ["APNSLiveActivityLogs"],
-      description : "get all APNS Live Activity Token Data",
-      secaurity : [{bearerAuth : []}]
+  getAll: {
+    schema: {
+      tags: ["APNSLiveActivityLogs"],
+      description: "get all APNS Live Activity Token Data",
+      secaurity: [{ bearerAuth: [] }],
+      body: {
+        type: "object",
+        properties: {
+          isActive: { type: "boolean" },
+          limit: { type: "integer" },
+          page: { type: "string" },
+          startDate: { type: "string" },
+          endDate: { type: "string" },
+          envType: { type: "integer" },
+          clientSocketId: { type: "integer" }
+        },
+        required: ["isActive", "limit", "page"]
+      }
     }
   }
 }
